5a5cba36709687a921b160c6cb02eec2da30042b
IvanDom93 <ivan.cba93@gmail.com>
2021-06-09 22:36:03 -0300

    Se agregaron los switch anidados

--- Libreria/src/paquete/Main.java
+++ Libreria/src/paquete/Main.java
@@ -24,12 +24,9 @@ public class Main {
         listadoRevista[2] = new Revistas("Revista Rumbos n354",400F, "2021-02-16");
 
         System.out.println("Seleccione la opcion que desee realizar");
-        System.out.println("1 - Listado total de libros");
-        System.out.println("2 - Listado total de comics");
-        System.out.println("3 - Listado total de revistas");
-        System.out.println("4 - Busqueda por autores");
-        System.out.println("5- Busqueda por generos");
-        System.out.println("6- Busqueda por rango de precios");
+        System.out.println("1 - Listados totales de productos");
+        System.out.println("2 - Busquedas parciales");
+
         System.out.println("7- La concha de tu madre lcm");
 
         Scanner scanner = new Scanner(System.in);
@@ -38,68 +35,83 @@ public class Main {
 
         switch (respuesta){
             case 1:
-                /*for(int i = 0;i < listadoLibros.length;i++){
-                    System.out.println(listadoLibros[i].toString());
-                }*/
-                for(Libros todoLibros:listadoLibros){
-                    //for each recorre todos los miembros del array, el if es para que no recorra los arrays vacios y no salte el .NullPointerException
-                    if(todoLibros == null) {
-                    break;
-                   }else {
-                        System.out.println(todoLibros.toString());
-                         }
-                   }
+                System.out.println("1 - Listado total de libros");
+                System.out.println("2 - Listado total de comics");
+                System.out.println("3 - Listado total de revistas");
+                Scanner scannerTotal = new Scanner(System.in);
+                System.out.print("Opcion: ");
+                int listados = scannerTotal.nextInt();
+                switch (listados){
+                    case 1:
+                        for(Libros todoLibros:listadoLibros){
+                            //for each recorre todos los miembros del array, el if es para que no recorra los arrays vacios y no salte el .NullPointerException
+                            if(todoLibros == null) {
+                                break;
+                            }else {
+                                System.out.println(todoLibros.toString());
+                            }
+                        }
+                        break;
+                    case 2:
+                        for(Comics todoComics:listadoComics){
+                            //for each recorre todos los miembros del array, el if es para que no recorra los arrays vacios y no salte el .NullPointerException
+                            if(todoComics == null) {
+                                break;
+                            }else {
+                                System.out.println(todoComics.toString());
+                            }
+                        }
+                        break;
+                    case 3:
+                        for(Revistas todoRevistas:listadoRevista){
+                            //for each recorre todos los miembros del array, el if es para que no recorra los arrays vacios y no salte el .NullPointerException
+                            if(todoRevistas == null) {
+                                break;
+                            }else {
+                                System.out.println(todoRevistas.toString());
+                            }
+                        }
+                        break;
+                    default:
+                        System.out.println("No ha elegido una opcion disponible");
+
+                }
                 break;
             case 2:
-                /*for(int i = 0;i < listadoComics.length;i++){
-                    System.out.println(listadoComics[i].toString());
-                }*/
-                for(Comics todoComics:listadoComics){
-                    //for each recorre todos los miembros del array, el if es para que no recorra los arrays vacios y no salte el .NullPointerException
-                    if(todoComics == null) {
-                        break;
-                    }else {
-                        System.out.println(todoComics.toString());
-                    }
-                }
-                break;
-            case 3:
-                /*for(int i = 0;i < listadoRevista.length;i++){
-                    System.out.println(listadoRevista[i].toString());
-                }*/
-                for(Revistas todoRevistas:listadoRevista){
-                    //for each recorre todos los miembros del array, el if es para que no recorra los arrays vacios y no salte el .NullPointerException
-                    if(todoRevistas == null) {
-                        break;
-                    }else {
-                        System.out.println(todoRevistas.toString());
-                    }
-                }
-                break;
-            case 4:
-                Scanner scanner1 = new Scanner(System.in);
-                System.out.print("Escribe el nombre del autor: ");
-                String nombreAutor = scanner1.nextLine().toUpperCase();
+                System.out.println("1 - Búsqueda por autores");
+                System.out.println("2 - Búsqueda por géneros");
+                System.out.println("3 - Búsqueda por rango de precios");
+                Scanner scannerBusqeda = new Scanner(System.in);
+                System.out.print("Opcion: ");
+                int busquedas = scannerBusqeda.nextInt();
 
-                int contador = 0;
-                for(Libros listadoAutores: listadoLibros){
-                   if(listadoAutores != null) {
-                      if(listadoAutores.getAutorNombre().toUpperCase().equals(nombreAutor)){
-                        System.out.println(listadoAutores.toString());
-                        contador++;
-                      }
-                   }
-                }
-                for(Comics listadoAutoresC: listadoComics){
-                   if(listadoAutoresC !=null){
-                      if(listadoAutoresC.getAutorNombre().toUpperCase().equals(nombreAutor)){
-                        System.out.println(listadoAutoresC.toString());
-                        contador++;
-                      }
-                   }
-                }
-                if(contador==0){
-                       System.out.println("No se ha encontrado ningun libro con ese autor");
+                switch (busquedas){
+                    case 1:
+                        Scanner scanner1 = new Scanner(System.in);
+                        System.out.print("Escribe el nombre del autor: ");
+                        String nombreAutor = scanner1.nextLine().toUpperCase();
+
+                        int contador = 0;
+                        for(Libros listadoAutores: listadoLibros){
+                            if(listadoAutores != null) {
+                                if(listadoAutores.getAutorNombre().toUpperCase().equals(nombreAutor)){
+                                    System.out.println(listadoAutores.toString());
+                                    contador++;
+                                }
+                            }
+                        }
+                        for(Comics listadoAutoresC: listadoComics){
+                            if(listadoAutoresC !=null){
+                                if(listadoAutoresC.getAutorNombre().toUpperCase().equals(nombreAutor)){
+                                    System.out.println(listadoAutoresC.toString());
+                                    contador++;
+                                }
+                            }
+                        }
+                        if(contador==0){
+                            System.out.println("No se ha encontrado ningun libro con ese autor");
+                        }
+                        break;
                 }
                 break;
             default:
